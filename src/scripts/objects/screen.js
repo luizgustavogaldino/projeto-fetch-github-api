dfffe2af3cc9fb38acbe2511b7ce64a7411a0dd0
Luiz Gustavo <luizgustavogaldino@gmail.com>
2024-08-04 14:08:44 -0300

    validacao de input e usuario

--- src/scripts/objects/screen.js
+++ src/scripts/objects/screen.js
@@ -22,7 +22,9 @@ const screen = {
                                            </div>`
 
         }
-
+    },
+    renderNotFound(){
+        this.userProfile.innerHTML = "<h3> Usuário não encontrado </h3>"
     }
 }
 
